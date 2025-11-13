f8003996de5c84adb2f0efc75655fc130bc68d0e
le86ksa <le86ksa@gmail.com>
2025-11-13 20:26:09 +0300

    animation1

--- app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -1,7 +1,9 @@
 package ru.netology.nmedia.ui
 
 import android.os.Bundle
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,7 +24,7 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-val view = findViewById<StatsView>(R.id.stats)
+        val view = findViewById<StatsView>(R.id.stats)
         binding.stats.data = listOf(
             500F,  // Фиолетовый - 25% (первый - перекрывает)
             500F,  // Бирюзовый - 25%
@@ -30,9 +32,26 @@ val view = findViewById<StatsView>(R.id.stats)
             500F,  // Розово-красный - 25% (последний)
         )
 
+        val textView = findViewById<TextView>(R.id.label)
+
         view.startAnimation(
-            AnimationUtils.loadAnimation(this, R.anim.animation)
+            AnimationUtils.loadAnimation(this, R.anim.animation).apply {
+                setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationEnd(animation: Animation?) {
+                        textView.text = "AnimationEnd"
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+                        textView.text = "AnimationRepeat"
+                    }
+
+                    override fun onAnimationStart(animation: Animation?) {
+                        textView.text = "AnimationStart"
+                    }
+
+                })
+            }
         )
-             // binding.stats.overlap = 4F // Оптимальное перекрытие
+        // binding.stats.overlap = 4F // Оптимальное перекрытие
     }
 }
